f65d5bcce4198ebf8762c2532668da7c18477e76
simon <raguins@hotmail.fr>
2020-06-10 11:22:25 +0200

    premier commit, camera + voiture ok

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float timeOffset;
+    public float rotationSpeed;
+
+    private Vector3 velocity;
+    private float timeCount = 0.0f;
+
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, timeOffset);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, Time.time * rotationSpeed); //rotSpeed = 10
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationSpeed);
+        timeCount = timeCount + Time.deltaTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
